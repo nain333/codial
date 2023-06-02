11b75798fc473f286ef0329af579fdaa3a6b9922
@nain333 <hnain70@gmail.com>
2023-06-02 18:15:52 +0530

    add a controller and a rounter for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = function(req,res){
+    res.send('<h1>The express is up and running for codial!</h1>')
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require ('express')
 const app = express();
-const port = 3400;
+const port = 3000;
+// use express router
+app.use('/', require('./routes/index'))
+
 app.listen(port,(error)=>{
     if(error){
         console.log(error);
