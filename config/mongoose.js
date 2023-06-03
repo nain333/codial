3fa6305e2abb1d6e926e7cb045614957533fbca0
@nain333 <hnain70@gmail.com>
2023-06-03 23:04:02 +0530

    connect to mongoDB using mangoose

---
+++ config/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose= require('mongoose');
+mongoose.connect('mongodb://localhost/codial-development-db')
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"Error while connecting to the db"));
+db.once('open',()=>{
+    console.log("Successfuly connected :: mongoDB")
+})
