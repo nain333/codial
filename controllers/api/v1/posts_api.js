7df2640f931fd341462797e6cdaa01cb2a750b42
@nain333 <hnain70@gmail.com>
2023-07-13 23:30:59 +0530

    Add api to the site with version set up and set up post man and make an api call to fetch all the posts from server and delte a post with no authentication

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,52 @@
+const user = require('../../../models/user')
+const Post = require('../../../models/post')
+const Comment = require('../../../models/comment')
+module.exports.index = async function(req,res){
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    
+    .populate({ 
+         path:'comments',
+         populate: {
+             path:  'user'
+         }
+
+    })
+    return res.json(200,{
+        message:'List of posts',
+        posts:posts
+    })
+
+}
+module.exports.destroy=async function(req,res){
+    try {
+        let post=await Post.findById(req.params.id);
+        // console.log(post.user)
+        
+        // if(post.user==req.user.id){
+            post.deleteOne();
+
+            Comment.deleteMany({
+                post:req.params.id
+             })
+           
+            // req.flash('success','post got deleted along with all associated comments')
+            
+            return res.status(200).json({
+                massage:'Post got deleted with all associated comments'
+            })
+        // }
+        // else{
+        //     req.flash('error',"you can't delete the post")
+        //     return res.redirect('/')
+            
+        // }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:'Internal server Error'
+        })
+    }
+    
+}
---
+++ controllers/api/v2/post_api.js
@@ -0,0 +1,12 @@
+
+
+module.exports.index=(req,res)=>{
+    return res.status(200).json({
+        data:{
+            postsList:[]
+        },
+        timeStamps:true
+        
+    })
+
+}
